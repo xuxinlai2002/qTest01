00acd881f0d28a0207c52412553719a0fbf787d1
xuxinlai <xuxinlai2002@gmail.com>
2020-08-23 23:48:34 +0000

    add

--- startAll.sh
+++ startAll.sh
@@ -4,6 +4,6 @@ cd node0
 PRIVATE_CONFIG=ignore nohup geth --datadir data \
 --nodiscover --istanbul.blockperiod 1 --syncmode full \
 --mine --minerthreads 1 --verbosity 5 --networkid 10  \
---rpc --rpcaddr 127.0.0.1 --rpcport 20648 --rpccorsdomain "*" \
+--rpc --rpcaddr 127.0.0.1 --rpcport 20646 --rpccorsdomain "*" \
 --rpcvhosts "*" --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul \
 --cryptomode gm --emitcheckpoints --port 20648 2>>node.log &
