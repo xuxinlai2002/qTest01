978d85e080f00ac966f3bd67d91d72e2c6263a9c
xuxinlai <xuxinlai2002@gmail.com>
2020-08-24 15:39:01 +0000

    add

---
+++ 3node/clear.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd node0
+rm -rf node.log
+rm -rf nohup.out
+rm -rf data/geth.ipc
+rm -rf data/geth/chaindata
+rm -rf data/geth/lightchaindata
+rm -rf data/geth/nodes
+rm -rf data/geth/LOCK
+rm -rf data/geth/transactions.rlp
+
