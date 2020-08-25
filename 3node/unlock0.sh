2b394df808a3997cce8b135b83c0fb3f55e56685
xuxinlai <xuxinlai2002@gmail.com>
2020-08-25 02:47:41 +0000

    add

---
+++ 3node/unlock0.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x2b977c7452b8494e0f83120dcdec129961b8f7c1","",10000],"id":1}' http://127.0.0.1:20636
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x20070c78b0420fb4e318eea569bca34c8aff553c","",10000],"id":1}' http://127.0.0.1:20636
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0xb06c8a2686694ffd80bd717409a66920515e028c","",10000],"id":1}' http://127.0.0.1:20636
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0xdbdea6091345c5499672fad52e6fc1fbc88520d8","",10000],"id":1}' http://127.0.0.1:20636
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x4601186425f0db04671430d01f0c4ba147df8c41","",10000],"id":1}' http://127.0.0.1:20636
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x4e28f4d005bc92d79a6a0301f6dfa12fe455fb3a","",10000],"id":1}' http://127.0.0.1:20636
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0xa6981d2915227f5b50e0da90dd2af07c1c75bd06","",10000],"id":1}' http://127.0.0.1:20636
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x76bc96a70d76c251e65c02893092448c0ef95e88","",10000],"id":1}' http://127.0.0.1:20636
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x105dd0e605c7c5dbfbdd29d801d8ce830df5733c","",10000],"id":1}' http://127.0.0.1:20636
+
