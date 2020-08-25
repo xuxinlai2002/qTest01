d11c782aa4cab479658d47aa8b0a9f9c91fb6437
xuxinlai <xuxinlai2002@gmail.com>
2020-08-25 03:39:52 +0000

    add

---
+++ 3node/sendInit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"0xe0f329c97ed0a219f9a82a0b494adec6df4b673c","to":"0x5fd072804a120eca6ea81eb7517395ad726c932b","value":"0x100000000"}],"id":1}' http://127.0.0.1:20646
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"0xe0f329c97ed0a219f9a82a0b494adec6df4b673c","to":"0xc2c3edef5ef8041f30396311de9cf953ad7c8840","value":"0x100000000"}],"id":1}' http://127.0.0.1:20646
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"0xe0f329c97ed0a219f9a82a0b494adec6df4b673c","to":"0xf241bc252b9009e40549d2a1981280597fc81872","value":"0x100000000"}],"id":1}' http://127.0.0.1:20646
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"0xe0f329c97ed0a219f9a82a0b494adec6df4b673c","to":"0xc41cce4e6e9e8a3b135048913e52a7b4ca181902","value":"0x100000000"}],"id":1}' http://127.0.0.1:20646
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"0xe0f329c97ed0a219f9a82a0b494adec6df4b673c","to":"0x8d7053f35ceacf587b857c1c0ab3149aca87f34b","value":"0x100000000"}],"id":1}' http://127.0.0.1:20646
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"0xe0f329c97ed0a219f9a82a0b494adec6df4b673c","to":"0xe24e63e808075c174d7b9cc5aa4b09f8e25db24a","value":"0x100000000"}],"id":1}' http://127.0.0.1:20646
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"0xe0f329c97ed0a219f9a82a0b494adec6df4b673c","to":"0x91a5851f3a4fe1a013ed00e1284e5800eb4c2a25","value":"0x100000000"}],"id":1}' http://127.0.0.1:20646
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"0xe0f329c97ed0a219f9a82a0b494adec6df4b673c","to":"0xbafef40980c69dfb0e122735700bb1fc97c5aaf0","value":"0x100000000"}],"id":1}' http://127.0.0.1:20646
+curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"0xe0f329c97ed0a219f9a82a0b494adec6df4b673c","to":"0xfa262fcc94842a899ffbb5fa9b27860105563590","value":"0x100000000"}],"id":1}' http://127.0.0.1:20646
+
