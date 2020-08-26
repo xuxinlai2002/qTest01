a7a9f9bbe5a5fb589d2dbae5ec689a1f39a56693
xuxinlai <xuxinlai2002@gmail.com>
2020-08-26 08:42:40 +0000

    addd

--- 3node/unlock1.sh
+++ 3node/unlock1.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x7ccd4ee1a450ce21e831a6c4bd15703be2d7c6b2","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x157e946b61804baed074dd79e492777616e5c69a","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x1adea40593fe7f429785e41af322c2f00a322247","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0xb7e06fd80138f42ce184306c98d9f3ac145f93be","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x1d8f79e3b0c9ef2b64ca74afe5edc403fa2b97d9","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x3aa8c22ce24d0a79f484eda6c7a39110762d8bad","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0xb66a3c2d5c6688b90623f371316f3f33877fc9a0","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0xab19c6ef367a6e8e67369a5873df4a13ace4208f","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0xf7f944447796bbbcf02cbd5326f1fe9321df1cca","",10000],"id":1}' http://127.0.0.1:20636 
 
--- 3node/unlock2.sh
+++ 3node/unlock2.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0xddbf03fe91e7ccae5d441db4e3ad6759d0f5ae99","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x8cf909a12ebd28a229e9b9db6f237cd927ca06c8","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x50af49c840ae02cfefd6275d146fc739b42f83c0","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x40a8d7e5779bd989cab8ac6fc0a70cedc981e4ac","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x5e01429525d06c28320749f6dbdf60e651cec175","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0xb62fcf1545504a8715af638b40c7a6ba055c5634","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0xeb985f863d47fcce168b57c6aef1090f55219a22","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x05751af5ec562b4ea38dc950eac599d6ff7df1c7","",10000],"id":1}' http://127.0.0.1:20636 
+sleep 2
 curl -H "Content-Type: application/json" -X POST  --data '{"jsonrpc":"2.0","method":"personal_unlockAccount","params":["0x3588f97197ba0984295604d4aeb3c70822d7d921","",10000],"id":1}' http://127.0.0.1:20636 
 
